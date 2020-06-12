14c90e5d581f2b1b00dfdfb213652a93b8fe0b4a
Anton Churkin <afrg@yandex.ru>
2020-06-12 12:52:58 +0500

    add propTypes

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import WelcomeScreen from './../welcome-screen/welcome-screen.jsx';
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {gameTime, errorCount} = props;
 
   return <WelcomeScreen
     time={gameTime}
     error={errorCount}
   />;
+};
+
+App.propTypes = {
+  gameTime: PropTypes.number.isRequired,
+  errorCount: PropTypes.number.isRequired,
 };
 
 export default App;
