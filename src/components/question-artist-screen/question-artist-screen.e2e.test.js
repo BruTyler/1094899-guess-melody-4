a726f970b230656c36cd4ee7da29dec609c9107e
Anton Churkin <afrg@yandex.ru>
2020-06-20 14:11:01 +0500

    transfer enzyme.config to detached file

--- src/components/question-artist-screen/question-artist-screen.e2e.test.js
+++ src/components/question-artist-screen/question-artist-screen.e2e.test.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import Enzyme, {shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import {shallow} from 'enzyme';
 import QuestionArtistScreen from './question-artist-screen.jsx';
-
-Enzyme.configure({
-  adapter: new Adapter(),
-});
 
 const QUESTION = {
   type: `artist`,
