7d9e51dc444ea483d6021ce5f74d0049ef98af00
Anton Churkin <afrg@yandex.ru>
2020-06-23 14:00:10 +0500

    component GameScreen is ready

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,8 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
+
+import GameScreen from './../game-screen/game-screen.jsx';
 import WelcomeScreen from './../welcome-screen/welcome-screen.jsx';
 import QuestionGenreScreen from '../question-genre-screen/question-genre-screen.jsx';
 import QuestionArtistScreen from '../question-artist-screen/question-artist-screen.jsx';
@@ -48,22 +50,27 @@ class App extends PureComponent {
       switch (question.type) {
         case (GameType.ARTIST):
           return (
-            <QuestionArtistScreen
-              onAnswer={this.handleAnswerSubmit}
-              question={question}
-            />);
+            <GameScreen gameType={question.type}>
+              <QuestionArtistScreen
+                onAnswer={this.handleAnswerSubmit}
+                question={question}
+              />
+            </GameScreen>
+          );
         case (GameType.GENRE):
           return (
-            <QuestionGenreScreen
-              onAnswer={this.handleAnswerSubmit}
-              question={question}
-            />);
+            <GameScreen gameType={question.type}>
+              <QuestionGenreScreen
+                onAnswer={this.handleAnswerSubmit}
+                question={question}
+              />
+            </GameScreen>);
         default:
-          return ``;
+          return null;
       }
     }
 
-    return ``;
+    return null;
   }
 
   render() {
@@ -75,14 +82,18 @@ class App extends PureComponent {
           {this._renderGameScreen()}
         </Route>
         <Route exact path="/dev-artist">
-          <QuestionArtistScreen
-            onAnswer={() => {}}
-            question={questions[1]}/>
+          <GameScreen gameType={GameType.ARTIST}>
+            <QuestionArtistScreen
+              onAnswer={() => {}}
+              question={questions[1]}/>
+          </GameScreen>;
         </Route>
         <Route exact path="/dev-genre">
-          <QuestionGenreScreen
-            onAnswer={() => {}}
-            question={questions[0]} />
+          <GameScreen gameType={GameType.GENRE}>
+            <QuestionGenreScreen
+              onAnswer={() => {}}
+              question={questions[0]}/>
+          </GameScreen>;
         </Route>
       </Switch>
     </BrowserRouter>;
@@ -93,11 +104,6 @@ App.propTypes = {
   gameTime: PropTypes.number.isRequired,
   errorCount: PropTypes.number.isRequired,
   questions: PropTypes.arrayOf(PropTypes.shape()).isRequired,
-};
-
-App.propTypes = {
-  gameTime: PropTypes.number.isRequired,
-  errorCount: PropTypes.number.isRequired,
 };
 
 export default App;
---
+++ src/components/game-screen/game-screen.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import GameScreen from './game-screen.jsx';
+import {GameType} from "../../const.js";
+
+const CHILDREN_COMPONENT = () => <div>test</div>;
+const GAME_TYPE = GameType.ARTIST;
+
+describe(`<GameScreen /> render suit`, () => {
+  it(`<GameScreen /> render case`, () => {
+    const generatedTree = renderer.create(
+        <GameScreen gameType={GAME_TYPE}>
+          {CHILDREN_COMPONENT}
+        </GameScreen>
+    ).toJSON();
+
+    expect(generatedTree).toMatchSnapshot();
+  });
+});
