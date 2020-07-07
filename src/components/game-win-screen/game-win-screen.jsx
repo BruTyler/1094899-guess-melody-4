fcc3fbc8360e07fe63534d2b430e082dd18f5a32
BruTyler <afrg@yandex.ru>
2020-07-07 10:17:52 +0500

    Update src/components/game-win-screen/game-win-screen.jsx
    Co-authored-by: Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>

--- src/components/game-win-screen/game-win-screen.jsx
+++ src/components/game-win-screen/game-win-screen.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GameWinScreen = ({onReplayButtonClick, mistakesCount, answeredQuestionsCount}) => {
-
-  return <section className="result">
+const GameWinScreen = ({onReplayButtonClick, mistakesCount, answeredQuestionsCount}) => (
+  <section className="result">
     <div className="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" /></div>
     <h2 className="result__title">Вы настоящий меломан!</h2>
     <p className="result__total">Вы ответили правильно на {answeredQuestionsCount} вопросов и совершили {mistakesCount} ошибки</p>
@@ -11,7 +10,7 @@ const GameWinScreen = ({onReplayButtonClick, mistakesCount, answeredQuestionsCou
       Сыграть ещё раз
     </button>
   </section>;
-};
+);
 
 GameWinScreen.propTypes = {
   onReplayButtonClick: PropTypes.func.isRequired,
