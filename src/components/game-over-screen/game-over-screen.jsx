bc2a83c83750c0df16013a9b37b2ce34c3d5ac8d
BruTyler <afrg@yandex.ru>
2020-07-07 10:17:31 +0500

    Update src/components/game-over-screen/game-over-screen.jsx
    Co-authored-by: Ruslan Malogulko <ruslan.malogulko@verygoodsecurity.com>

--- src/components/game-over-screen/game-over-screen.jsx
+++ src/components/game-over-screen/game-over-screen.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const GameOverScreen = ({onReplayButtonClick}) => {
-
-  return <section className="result">
+const GameOverScreen = ({onReplayButtonClick}) => (
+  <section className="result">
     <div className="result__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" /></div>
     <h2 className="result__title">Какая жалость!</h2>
     <p className="result__total result__total--fail">У вас закончились все попытки. Ничего, повезёт в следующий раз!</p>
@@ -11,7 +10,7 @@ const GameOverScreen = ({onReplayButtonClick}) => {
         Попробовать ещё раз
     </button>
   </section>;
-};
+);
 
 GameOverScreen.propTypes = {
   onReplayButtonClick: PropTypes.func.isRequired,
